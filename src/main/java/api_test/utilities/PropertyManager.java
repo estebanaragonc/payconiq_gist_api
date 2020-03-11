654dcbbab2d15ecc922b467b0804db78111fbaf8
Elvis Aragon <Elvis.X.Aragon.-ND@disney.com>
2020-03-11 13:48:48 -0500

    adding property file

---
+++ src/main/java/api_test/utilities/PropertyManager.java
@@ -0,0 +1,32 @@
+package api_test.utilities;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertyManager {
+
+	private static Properties properties;
+    private static final String PROPERTIES_FILE_PATH = "/application.properties";
+    
+    public PropertyManager () {
+    	properties = new Properties();
+        System.out.println ("PROPERTIES_FILE_PATH : " + PROPERTIES_FILE_PATH);
+        System.out.println(PropertyManager.class.getResource("/").getPath());
+        InputStream is = PropertyManager.class.getResourceAsStream(PROPERTIES_FILE_PATH);
+        System.out.println( "status" + is);
+        
+        try {
+            properties.load(is);
+        } catch (IOException e) { 
+            throw new RuntimeException("File path does not exit" + PROPERTIES_FILE_PATH, e);
+        }
+        
+    }
+    
+    //to obtain property absed in name
+    public static String get (String propertyName) {
+        return properties.getProperty(propertyName);
+    }
+    
+}
