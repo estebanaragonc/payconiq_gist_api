7eb361479798260c1b36e25d9418d089fad44752
Elvis Aragon <Elvis.X.Aragon.-ND@disney.com>
2020-03-11 14:09:08 -0500

    added get example

---
+++ src/test/java/api_test/test/GetExample.java
@@ -0,0 +1,24 @@
+package api_test.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.jayway.restassured.response.Response;
+
+import api_test.gist.Gist;
+import api_test.util.BaseTestClass;
+
+
+public class GetExample extends BaseTestClass{
+
+	private final String gistId = "54d4b4a68737a81c73b307309d9e1d4d";
+	
+	@Test(description = "List a user's gists")
+    public void listUsersGistsTest() {
+        Response response = getGivenAuth().get().andReturn();
+        Gist[] gists = response.as(Gist[].class);
+        Assert.assertEquals(response.statusCode(), 200);
+        Assert.assertTrue(gists.length > 0);
+    }
+	
+}
---
+++ src/test/java/api_test/util/BaseTestClass.java
@@ -0,0 +1,22 @@
+package api_test.util;
+
+import static com.jayway.restassured.RestAssured.given;
+
+import com.jayway.restassured.builder.RequestSpecBuilder;
+import com.jayway.restassured.specification.RequestSpecification;
+
+import api_test.utilities.PropertyManager;
+
+public class BaseTestClass {
+
+	private final String token = PropertyManager.get("git.token");
+    private final String baseUrl = "https://api.github.com/gists";
+    private final RequestSpecification requestSpecification = new RequestSpecBuilder().setBaseUri(baseUrl).build();
+    
+    
+    //to handle auth based in token provided by property file
+    protected RequestSpecification getGivenAuth() {
+        return given().spec(requestSpecification).auth().oauth2(token);
+    }
+    
+}
